3d2067284f96cb09879b6f842b8de186c2d664b8
Terry1121 <teru19911121@gmail.com>
2021-12-17 16:27:31 +0900

    修正内容が分かるコミットメッセージ

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,6 +7,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="reset.css" />
     <style>
+
+  .flex {
+      display: flex;
+    }
+
+    .between {
+      justify-content: space-between;
+    }
+
+    .tlt-1 {
+      margin-bottom: 15px;
+    }
+
+    .tlt-2 {
+      margin-bottom: 30px;
+    }
+
     .container {
       background-color: #2d197c;
       height: 100vh;
@@ -14,119 +31,132 @@
       position: relative;
     }
 
-    .card{
-    background-color: #fff;
-    width: 50vw;
-    padding: 30px;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    border-radius: 10px;
+    .card {
+      background-color: #fff;
+      width: 50vw;
+      padding: 30px;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      border-radius: 10px;
     }
 
-
-    .todo-title{
+    .title {
       font-weight: bold;
       font-size: 24px;
     }
 
-    .btn-add{
-    text-align: left;
-    border: 2px solid #dc70fa;
-    font-size: 12px;
-    color: #dc70fa;
-    background-color: #fff;
-    font-weight: bold;
-    padding: 8px 16px;
-    border-radius: 5px;
-    cursor: pointer;
-    transition: 0.4s;
-    outline: none;
+    .input-add {
+      width: 80%;
+      padding: 5px;
+      border-radius: 5px;
+      border: 1px solid #ccc;
+      appearance: none;
+      font-size: 14px;
+      outline: none;
     }
 
-    .todo-search{
-      margin-bottom: 30px;
-      justify-content: space-between;
-      display: flex;
+    table {
+      text-align: center;
+      width: 100%
     }
 
-    .form-control{
-    width: 80%;
-    padding: 5px;
-    border-radius: 5px;
-    border: 1px solid #ccc;
-    appearance: none;
-    font-size: 14px;
-    outline: none;
+    tr {
+      height: 50px;
     }
 
-    .btn-update{
-    text-align: left;
-    border: 2px solid #fa9770;
-    font-size: 12px;
-    color: #fa9770;
-    background-color: #fff;
-    font-weight: bold;
-    padding: 8px 16px;
-    border-radius: 5px;
-    cursor: pointer;
-    transition: 0.4s;
-    outline: none;
+    .update {
+      width: 90%;
+      padding: 5px;
+      border-radius: 5px;
+      border: 1px solid #ccc;
+      appearance: none;
+      font-size: 14px;
+      outline: none;
     }
 
-    .btn-delete{
-    text-align: left;
-    border: 2px solid #71fadc;
-    font-size: 12px;
-    color: #71fadc;
-    background-color: #fff;
-    font-weight: bold;
-    padding: 8px 16px;
-    border-radius: 5px;
-    cursor: pointer;
-    transition: 0.4s;
-    outline: none;
+    .button-add {
+      text-align: left;
+      border: 2px solid #dc70fa;
+      font-size: 12px;
+      color: #dc70fa;
+      background-color: #fff;
+      font-weight: bold;
+      padding: 8px 16px;
+      border-radius: 5px;
+      cursor: pointer;
+      transition: 0.4s;
+      outline: none;
     }
 
-    table{
-    text-align: center;
-    width: 100%;
+    .button-add:hover {
+      background-color: #dc70fa;
+      border-color: #dc70fa;
+      color: #fff;
     }
 
-    .table1{
-    margin: 0;
-    padding: 0;
-    border: 0;
-    outline: 0;
-    font-size: 100%;
-    vertical-align: baseline;
-    background: transparent;
+    .button-update {
+      text-align: left;
+      border: 2px solid #fa9770;
+      font-size: 12px;
+      color: #fa9770;
+      background-color: #fff;
+      font-weight: bold;
+      padding: 8px 16px;
+      border-radius: 5px;
+      cursor: pointer;
+      transition: 0.4s;
+      outline: none;
+    }
+
+    .button-update:hover {
+      background-color: #fa9770;
+      border-color: #fa9770;
+      color: #fff;
+    }
+
+    .button-delete {
+      text-align: left;
+      border: 2px solid #71fadc;
+      font-size: 12px;
+      color: #71fadc;
+      background-color: #fff;
+      font-weight: bold;
+      padding: 8px 16px;
+      border-radius: 5px;
+      cursor: pointer;
+      transition: 0.4s;
+      outline: none;
+    }
+
+    .button-delete:hover {
+      background-color: #71fadc;
+      border-color: #71fadc;
+      color: #fff;
     }
 
     </style>
 </head>
 
 <body>
-    <div class="container" style="margin-top:50px;">
+    <div class="container">
       <div class="card">
-        <p class="todo-title">Todo List<p>
+        <p class="title tlt-1">Todo List<p>
           <div class="todo">
    
-
-        <form action='/todo/create' method="post" class ="todo-search">
+        <form action='/todo/create' method="post" class ="flex between tlt-2">
             @csrf
             <div class="form-group">
-                 <input type="text" name="body" class="form-control" placeholder="todo list" style="max-width:1000px;">
+                 <input type="text" name="body" class="input-add" placeholder="todo list" style="max-width:1000px;">
             </div>
-            <button type="submit" class="btn-add">追加</button>
+            <button type="submit" class="button-add">追加</button>
         </form>
 
         
 
-        <table class="table1" style="max-width:700px; margin-top:50px;">
-         
-          <tr>
+        <table>
+          <tbody><tr>
             <th>作成日</th>
             <th>タスク名</th>
             <th>更新</th>
@@ -136,29 +166,27 @@
             @foreach ($todos as $todo)
             <tr>
                 <td>{{ $todo->created_at }}</td>
-                <td>{{ $todo->id }}</td>
+                <input type="hidden" name="id" value="{{ $todo->id }}">
                 <td>
-                
                     <form action="{{ route('todo.update', ['id' => $todo->id]) }}" method="post" >
                     @csrf
-                     <td>
-                    
-               <input type="text" value="{{ $todo->body }}" name="body" />
-                        <button type="submit" class="btn-update">更新</button>
-                       
+                        <input type="text"  class="update" value="{{ $todo->body }}" name="body" />
+                        <td><button type="submit" class="button-update">更新</button></td>
                     </form>
                 </td>
+                
                 <td>
                     <form action="{{ route('todo.delete', ['id' => $todo->id]) }}" method="post">
-                      
                      @csrf
-                        <button type="submit" class="btn-delete">削除</button>
+                        <button type="submit" class="button-delete">削除</button>
                     </form>
                 </td>
             </tr>
             @endforeach
         </table>
+
     </div>
+     </div>
 </body>
 
 </html>
